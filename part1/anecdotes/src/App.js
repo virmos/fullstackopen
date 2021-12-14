dcc7aaa02eb4819f0d89ae902d82760c03db3d20
tipro1810 <30485720+tipro1810@users.noreply.github.com>
2021-12-14 15:20:47 +0700

    finish part 1

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -1,25 +1,101 @@
-import logo from './logo.svg';
+import React, { useState } from 'react'
 import './App.css';
 
+function Button({onClick, text, currentIndex, arrayLength}) {
+  if (currentIndex < arrayLength - 1) {
+    return (
+      <button onClick={onClick}>
+          {text}
+      </button>
+    )
+  } else {
+    return (
+      <button> That was all anecdotes </button>
+    )
+  }
+}
+
+function History({anecdotes, votesDict, mostVotedIndex}) {
+  if (mostVotedIndex !== -1) {
+    return (
+      <div>
+        <div> {anecdotes[mostVotedIndex]} </div>
+        <div> has {votesDict[mostVotedIndex]} votes </div>
+      </div>
+    )
+  }else {
+    return (
+      <div></div>
+    )
+  }
+}
+
+const anecdotes = [
+  'If it hurts, do it more often',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+  'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients'
+]
+
+const shuffleArray = (array) => {
+  let currentIndex = array.length - 1
+  while (currentIndex >= 0) {
+    let randomIndex = Math.floor(Math.random() * currentIndex)
+    let temp = array[currentIndex]
+    array[currentIndex] = array[randomIndex]
+    array[randomIndex] = temp
+
+    currentIndex --
+  }
+}
+
+shuffleArray(anecdotes)
+
 function App() {
+  function argmax(array) {
+    if (array.length === 0) { return null }
+    var max = array[0]
+    var maxIndex = 0
+
+    for (var i = 1; i < array.length; i++) {
+        if (array[i] > max) {
+            maxIndex = i
+            max = array[i]
+        }
+    }
+    return maxIndex
+  }
+
+  const [selected, setSelected] = useState(0)
+  const [votesDict, setVotesDict] = useState(new Uint8Array(anecdotes.length))
+  const [mostVotedIndex, setMostVotedIndex] = useState(-1)
+
+  const chooseNextAnecdote = () => setSelected(selected + 1)
+  const vote = () => {
+    const newVotesDict = [...votesDict]
+    newVotesDict[selected] = newVotesDict[selected] + 1
+    setVotesDict(newVotesDict)
+    setMostVotedIndex(argmax(newVotesDict))
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2> Anecdotes of the day </h2>
+      <div> {anecdotes[selected]} </div>
+      <div> has {votesDict[selected]} votes </div>
+      <Button onClick={vote} text={"vote"}
+              currentIndex={selected} arrayLength={anecdotes.length}/>
+      <Button onClick={chooseNextAnecdote} text={"next anecdote"}
+              currentIndex={selected} arrayLength={anecdotes.length}/>
+
+      <h2> Anecdotes with most votes </h2>
+      <History anecdotes={anecdotes} votesDict={votesDict} mostVotedIndex={mostVotedIndex}/>
+      
     </div>
-  );
+  )
 }
 
 export default App;
--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import Button from "./Button.js"
 
-function App() {
+const StatisticLine = ({text, value, type}) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <tr> 
+      <td> {text} </td>
+      <td> {value}{type} </td>
+    </tr>
+  )
+}
+
+const Statistics = ({good, neutral, bad}) => {
+  const average = (good - bad)/(good + neutral + bad)
+  const positivePercentage = good / (good + neutral + bad) * 100
+
+  if (good + neutral + bad !== 0) {
+    return (
+      <table>
+        <tbody>
+          <StatisticLine text={"good"} value={good}/>
+          <StatisticLine text={"neutral"} value={neutral}/>
+          <StatisticLine text={"bad"} value={bad}/>
+          <StatisticLine text={"average"} value={average}/>
+          <StatisticLine text={"positive"} value={positivePercentage} type={"%"}/>
+        </tbody>
+      </table>  
+    )
+  } else {
+    return (
+      <div> No feedback given </div> 
+    )
+  }
+}
+
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const increaseGoodByOne = () => setGood(good + 1) 
+  const increaseNeutralByOne = () => setNeutral(neutral + 1) 
+  const increaseBadByOne = () => setBad(bad + 1)
+
+  return (
+    <div>
+      <h2>Give feedback</h2>
+      <Button onClick={increaseGoodByOne} text={"good"}/>
+      <Button onClick={increaseNeutralByOne} text={"neutral"}/>
+      <Button onClick={increaseBadByOne} text={"bad"}/>
+      <h2>Statistics</h2> 
+      <Statistics good={good} neutral={neutral} bad={bad}/>
     </div>
-  );
+  )
 }
 
 export default App;
